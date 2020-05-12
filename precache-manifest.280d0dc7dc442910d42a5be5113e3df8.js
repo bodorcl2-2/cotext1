26c17b92d7a2f04adffe38640d3f68ee50cb59bb
darekrcl <darek.rcl@gmail.com>
2020-05-12 17:53:14 +0200

    Updates

---
+++ precache-manifest.280d0dc7dc442910d42a5be5113e3df8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77b8702c9feb56eabecb5a6bba44b14",
+    "url": "/cotext1/index.html"
+  },
+  {
+    "revision": "f4d87209731c318df3d1",
+    "url": "/cotext1/static/css/main.51cc8924.chunk.css"
+  },
+  {
+    "revision": "b605232264c827cf6253",
+    "url": "/cotext1/static/js/2.cec5dd8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cotext1/static/js/2.cec5dd8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d87209731c318df3d1",
+    "url": "/cotext1/static/js/main.150c963b.chunk.js"
+  },
+  {
+    "revision": "2c826309d32c66b82c45",
+    "url": "/cotext1/static/js/runtime-main.f6c3cc97.js"
+  }
+]);
